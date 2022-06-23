d1f87df61c1b4a8673529f61220f6ccf8a241e0f
victorynifemi <alagbekehindevictory@gmail.com>
2022-06-23 23:44:28 +0100

    About

---
+++ index.js
@@ -0,0 +1,7 @@
+function my_details(name, height, country) {
+    let name = "Alagbe Kehinde Victory"
+    let height = "160cm"
+    let country = 'Nigeria'
+     alert("Here are my details:  My name is :" + " " + name +  " ; " + "I am" + " " + height + " tall " + "; " + "and my Country is" + " " + country)
+}
+my_details()
