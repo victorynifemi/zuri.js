7a827808c26578feee990c7e26fc016162e7b0fe
Victory <105557164+victorynifemi@users.noreply.github.com>
2022-06-24 21:23:09 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-function my_details(name, height, country) {
+function my_details() {
     let name = "Alagbe Kehinde Victory"
     let height = "160cm"
     let country = 'Nigeria'
-     alert("Here are my details:  My name is :" + " " + name +  " ; " + "I am" + " " + height + " tall " + "; " + "and my Country is" + " " + country)
+     alert("Here are my details:  My name is :" + " " + name +  " \n " + "I am" + " " + height + " tall " + "\n " + "and my Country is" + " " + country)
 }
-my_details()
